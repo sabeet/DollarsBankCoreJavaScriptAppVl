5cc640b75f2ef19df85b36b12087286fd1893f03
sabeet <sabeet.other.way@gmail.com>
2021-09-08 12:46:59 -0400

    First big change

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log("Let the games begin")
+var readline = require('readline');
+
+var rl = readline.createInterface(process.stdin, process.stdout)
+
+rl.question("Enter a name.", function(input){
+    console.log("You inputted " + input)
+});
